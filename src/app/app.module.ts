5264f32f1d50455e923cfcd4b37b2e98b13f5676
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-14 22:14:23 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+//simplified API for Angular applications that makes it 
+//possible for the client app to communicate with the 
+//API or server-side
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
