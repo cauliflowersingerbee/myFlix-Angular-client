49fbbcb23b0f5f9eb4235c76951f074570cd09c8
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-15 13:20:12 +0100

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://kino-noir.herokuapp.com';
 @Injectable({
   providedIn: 'root'
 })
@@ -17,10 +17,21 @@ export class UserRegistrationService {
  // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails)
+    return this.http
+    .post(apiUrl + 'users', userDetails)
     //The pipe() function takes the functions you want to combine (in this case, 
     //there's one method, catchError) as its arguments and will return a new 
     //function that, when executed, runs the composed functions in sequence.
+    .pipe(
+    catchError(this.handleError)
+    );
+  }
+
+   // Making the api call for the user login endpoint
+   public userLogin(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+    .post(apiUrl + 'users', userDetails)
     .pipe(
     catchError(this.handleError)
     );
