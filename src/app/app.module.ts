eb5e0a4446829bf4d428e11ef95c8be1cc51b476
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-16 14:55:56 +0100

    added modules from material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
