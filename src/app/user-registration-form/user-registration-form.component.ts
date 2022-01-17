91099e7c6756ed40ea80b585f9d616715d606fa0
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-17 22:50:35 +0100

    Update user-registration-form.component.ts

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -38,7 +38,7 @@ registerUser(): void {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
      console.log(response);
-     this.snackBar.open(response, 'OK', {
+     this.snackBar.open('user registered successfully', 'OK', {
         duration: 2000
      });
     }, (response) => {
