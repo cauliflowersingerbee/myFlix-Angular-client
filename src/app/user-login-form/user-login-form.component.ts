5fdbd3a8301721131c4eb373a07dd832bdd1345d
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-18 10:50:57 +0100

    updated files

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -23,7 +23,7 @@ export class UserLoginFormComponent implements OnInit {
   ngOnInit(): void {
   }
 // This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
+loginUser(): void {
   this.fetchApiData.userLogin(this.userData).subscribe((response) => {
 // Logic for a successful user registration goes here! (To be implemented)
    this.dialogRef.close(); // This will close the modal on success!
