3080e25bdd733e20da4eedce1703148f77dc231d
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-18 10:38:25 +0100

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -13,11 +13,32 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class UserLoginFormComponent implements OnInit {
 
-
+  @Input() userData = { Username: '', Password: ''};
   
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
+// This is the function responsible for sending the form inputs to the backend
+registerUser(): void {
+  this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+// Logic for a successful user registration goes here! (To be implemented)
+   this.dialogRef.close(); // This will close the modal on success!
+   console.log(response);
+   this.snackBar.open('user logged in successfully', 'OK', {
+      duration: 2000
+   });
+  }, (response) => {
+    console.log(response)
+    this.snackBar.open(response, 'OK', {
+      duration: 2000
+    });
+  });
+}
 
 }
+
+
