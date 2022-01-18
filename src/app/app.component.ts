13a66c80b6e1e9c13575a8346ff2fcd7a9c2aea7
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-18 09:32:16 +0100

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
