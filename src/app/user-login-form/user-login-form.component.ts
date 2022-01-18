b136dccbfed9f815dbb18ee10c192d7bc52b5ea1
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-18 10:27:43 +0100

    made necessary imports

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.css']
+  styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
 
+
+  
   constructor() { }
 
   ngOnInit(): void {
