7d3d4ab65e79800de0341272f26678c2148a1fd5
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-18 10:05:23 +0100

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -14,6 +14,7 @@ export class FetchApiDataService {
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
   }
+  
  // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
