d7575853010df60f9a1a1e98970b55433cf446b7
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-19 10:17:39 +0100

    file updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,12 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
@@ -39,8 +39,8 @@ const appRoutes: Routes = [
     MovieCardComponent,
     WelcomePageComponent,
     UserProfileComponent,
-    NavbarComponent
   ],
+  
   imports: [
     BrowserModule, 
     HttpClientModule, 
@@ -53,7 +53,7 @@ const appRoutes: Routes = [
     MatSnackBarModule,
     FormsModule,
     RouterModule.forRoot(appRoutes), 
-    MatIconModule
+    MatIconModule,
 
   ],
   providers: [],
