2f0d40bc2767307cf0e497bbebe66e38539e4796
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-19 14:15:35 +0100

    imported dependencies

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,10 @@
 //allows users to view/edit their profile.
 
 import { Component, OnInit } from '@angular/core';
-
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
@@ -9,7 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public router: Router,
+    public snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
   }
