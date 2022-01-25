553ab80977161c15f6623ada9bc1e4acbf624758
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-25 12:13:26 +0100

    updates

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(@Inject(MAT_DIALOG_DATA) 
+  public data: { Title: string, Description: string }) { }
+  
   ngOnInit(): void {
   }
 
