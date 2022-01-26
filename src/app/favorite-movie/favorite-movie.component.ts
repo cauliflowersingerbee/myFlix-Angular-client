f90fd67a841b652dd1bda3009c03e054dc915ade
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-26 12:09:38 +0100

    Update favorite-movie.component.ts

--- src/app/favorite-movie/favorite-movie.component.ts
+++ src/app/favorite-movie/favorite-movie.component.ts
@@ -51,8 +51,8 @@ export class FavoriteMovieComponent implements OnInit {
         (this.movies.map((movie: any) => {movie._id ===
           this.favorites.find((fav) => fav === movie._id)}
     ))) {
-      console.log(this.movies.Title);
-      return this.movies.Title;
+      console.log(this.movies);
+      return this.movies;
     }
    })
   }
