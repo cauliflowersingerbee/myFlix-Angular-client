9a49f6eab6319a88e0672e86d7f3d1bfcf626d37
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-27 12:56:53 +0100

    Update favorite-movie.component.ts

--- src/app/favorite-movie/favorite-movie.component.ts
+++ src/app/favorite-movie/favorite-movie.component.ts
@@ -15,7 +15,10 @@ export class FavoriteMovieComponent implements OnInit {
   //user: any = JSON.parse(localStorage.getItem('user') || '');
   user: any = {};
   movies: any = [];
-  favorites: any[] = [];
+  //list of favorite ids
+  favoriteIds: any[] = [];
+  //list of favorite movies
+  favoriteMoviesList: any[] = [];
   
   constructor(
     public fetchApiData: FetchApiDataService,
@@ -25,15 +28,19 @@ export class FavoriteMovieComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    //this.getMovies();
     this.getFavorites();
-    
+    this.getUserDetails()
   }
 
   getUserDetails() : void {
     const user = JSON.parse(localStorage.getItem('user') || '');
     this.fetchApiData.getUser(user.Username).subscribe((res: any) => {
       this.user = res;
+      //adding list of favorite movies to local state
+      this.favoriteIds = res.FavoriteMovie;
+      console.log(this.user);
+      //adding movies to local state
+      this.getMovies();
     });
   }
 
@@ -42,32 +49,18 @@ export class FavoriteMovieComponent implements OnInit {
     .subscribe((resp: any) => {
         this.movies = resp;
         console.log(resp);
-        
+        //calling favorites
+        this.getFavorites()
       });
     }
 
     getFavorites() : void {
-      //const user = JSON.parse(localStorage.getItem('user') || '');
-      this.getMovies();
-      this.fetchApiData.getFavorites().subscribe((resp: any) => {
-        this.favorites = resp;
-        console.log(resp);
-        if (this.favorites.length === 0) 
-          {return 'You have no favorite movies'} 
-    
-      else if (this.movies.map((movie: any) => {movie._id ===
-       this.favorites.find((fav) => fav === movie._id);
-      })) 
-          {let array = this.favorites.map((fav) => 
+          let favoriteMoviesList = this.favoriteIds.map((fav) => 
             {return this.movies.find((movie: any) => movie._id === fav);
           });
-    console.log(array) ;
-    return array;
-    }
-    return
-      }
-      )
-
+          //adding movieIDs to local state
+          this.favoriteMoviesList = favoriteMoviesList;
+          console.log(this.favoriteMoviesList);
     }
     
 
