63d1093131906a7a7aadb22457bf70f5b772673d
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-01-31 21:35:11 +0100

    Update movie-director.component.ts

--- src/app/movie-director/movie-director.component.ts
+++ src/app/movie-director/movie-director.component.ts
@@ -1,3 +1,10 @@
+/**
+ * @file contains logic to build movie director component. 
+ * UI will show a button that, when clicked, takes a user 
+ * to the director view, where details about the
+ director of that particular movie will be displayed.
+ */
+
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog'; 
 
@@ -8,7 +15,10 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./movie-director.component.css']
 })
 export class MovieDirectorComponent implements OnInit {
-
+   /**
+   *
+   * @param data
+   */
   constructor(
     @Inject(MAT_DIALOG_DATA)  
     public data: { Name: string, Bio: string, Birth: string, Death: string }
