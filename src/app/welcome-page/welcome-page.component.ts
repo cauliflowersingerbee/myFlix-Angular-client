b55708c6c76a91f72439999d44c819e759f5c9d8
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-01 10:09:39 +0100

    Update welcome-page.component.ts

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,3 +1,8 @@
+/**
+ * @file contains logic for welcome page where 
+ * user can either register an account or sign in 
+ * to their profile
+ */
 import { Component, OnInit } from '@angular/core';
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
@@ -12,11 +17,23 @@ export class WelcomePageComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
   ngOnInit(): void {
   }
+
+  /**
+   * logic to have a dialog box open where user can register
+   * for an account
+   * @method openUserRegistrationDialog
+   */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       width: '280px'
     });
   }
+
+  /**
+   * logic to have a dialog box open where user can login
+   * to their profile
+   * @method openUserLoginDialog
+   */
 openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
