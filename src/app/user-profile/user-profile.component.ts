20f11ec6e64bf5a2e6f55e88d44150c8d7774ca0
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-01 09:58:51 +0100

    commented user profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
-//allows users to view/edit their profile.
+/**
+ * @file contains logic that allows users to 
+ * view/edit their profile.
+ */
 
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
@@ -29,10 +32,18 @@ export class UserProfileComponent implements OnInit {
     public snackBar: MatSnackBar
   ) { }
 
+  /**
+   * On initializing, we get the user's details
+   */
   ngOnInit(): void {
     this.getUserDetails();
   }
 
+  /**
+   * getting the user's details
+   * @method getUserDetails
+   * Then we store details in local user state
+   */
   getUserDetails() : void {
     const user = JSON.parse(localStorage.getItem('user') || '');
     this.fetchApiData.getUser(user.Username).subscribe((res: any) => {
@@ -40,6 +51,11 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
+  /**
+   * logic to allow user to click a button and 
+   * delete their account
+   * @method deregisterUser
+   */
  
   deregisterUser(): void {
     this.dialog.open(ProfileDeleteComponent, {
@@ -47,12 +63,22 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
+  /**
+   * logic to allow user to click a button and 
+   * edit their account
+   * @method openEditProfileDialog
+   */
   openEditProfileDialog(): void {
     this.dialog.open(ProfileEditComponent, {
       width: '500px',
     });
   }
 
+  /**
+   * logic to allow user to click a button and 
+   * see their favorite movies
+   * @method openFavoriteMovieDialog
+   */
   openFavoriteMovieDialog(): void {
     this.dialog.open(FavoriteMovieComponent, {
       width: '500px',
