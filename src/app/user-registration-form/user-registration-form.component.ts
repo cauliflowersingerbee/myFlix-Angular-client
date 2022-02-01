b1a98f426c627ad417fcea862db05272ff04262a
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2022-02-01 10:04:57 +0100

    Update user-registration-form.component.ts

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,25 +1,26 @@
-// src/app/user-registration-form/user-registration-form.component.ts
+/**
+ * @file contains logic to allow a user to register
+ * for an account
+ */
+
 import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-//decorator to tell angular that class right below is a component
+
 @Component({
-  //selector defines the custom HTML element into which this 
-  //component will render
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
   styleUrls: ['./user-registration-form.component.scss']
 })
 export class UserRegistrationFormComponent implements OnInit {
-//decorator to define the component's input (user data)
+
+/**
+ * decorator to define the component's input and bind it to 
+ * userData object
+ */
+
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
 constructor(
@@ -27,12 +28,16 @@ constructor(
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar) { }
 
-//called once the component has received all its inputs 
-//(all its data-bound properties) from the calling component
+
 ngOnInit(): void {
 }
 
-// This is the function responsible for sending the form inputs to the backend
+/**
+ * we invoke the http request to register a user using the userData
+ * bound in the input. 
+ * @function registerUser
+ * Then we inform the user if they were successfully registered or not
+ */
 registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
   // Logic for a successful user registration goes here! (To be implemented)
